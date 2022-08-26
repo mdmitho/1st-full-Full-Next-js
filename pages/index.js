e7cbc2a792f1205786e01c685a8bc389a31ccb8f
Hossen <mdmitho199911@gmail.com>
2022-08-27 00:02:18 +0600

    Link componet create navbar without page Refresh

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Link from "next/link"
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>hello world</h1>
+      <nav>
+        <ul>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+          <Link href="/product">
+            <a >Product</a>
+          </Link>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
---
+++ pages/product.js
@@ -0,0 +1,13 @@
+
+
+
+
+const Product = () => {
+    return (
+        <div>
+            <h1>my Priduct</h1>
+        </div>
+    );
+};
+
+export default Product;
