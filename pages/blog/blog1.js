fc355781e9231c3c0449cd59917211fc7f130ed6
Hossen <mdmitho199911@gmail.com>
2022-08-26 11:20:36 +0600

    nested routing

---
+++ pages/blog/blog1.js
@@ -0,0 +1,10 @@
+
+const blog1 = () => {
+    return (
+        <div>
+            <h1>blog 1</h1>
+        </div>
+    );
+};
+
+export default blog1;
---
+++ pages/blog/blog2.js
@@ -0,0 +1,11 @@
+
+
+const blog2 = () => {
+    return (
+        <div>
+            <h1>blog 2</h1>
+        </div>
+    );
+};
+
+export default blog2;
