db5ad53ab89a346b84a2432b67b6178e61ab29e6
Hossen <mdmitho199911@gmail.com>
2022-08-27 19:19:29 +0600

    img components optimization

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import Image from 'next/image'
 
 const About = () => {
     return (
-        <div>
-            <Navbar/>
-            <h1>About</h1>
-        </div>
+      <div style={{textAlign:"center"}}>
+        <Navbar />
+        <h1>About</h1>
+        {/* <Image src='/logo2.png'
+            layout='fill'
+            >
+
+            </Image> */}
+        <Image src="/logo2.png" width="500" height="300"></Image>
+        
+        <Image
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGCjHYhrksXwIZZwZJlx1jk2JBIcH10v7LZw&usqp=CAU"
+          width="500"
+          height="300"
+        ></Image>
+      </div>
     );
 };
 
